763f6646361d9d15bf99ec9e51e3d1e7918f45c4
Aryyadi72 <aryyadisetiawan002@gmail.com>
2024-11-18 16:19:11 +0800

    long time no commit

--- resources/views/main-app.blade.php
+++ resources/views/main-app.blade.php
@@ -40,99 +40,6 @@
         </div>
 
         {{-- <div class="row">
-            <!-- Column -->
-            <div class="col-md-6 col-lg-3">
-                <div class="card card-hover">
-                    <a href="http://192.168.99.202/absen/public" target="_blank">
-                        <div class="box bg-dark text-center">
-                            <h1 class="font-light text-white"><i class="mdi mdi-account-multiple"></i></h1>
-                            <h6 class="text-white">Attendance Management</h6>
-                        </div>
-                    </a>
-                </div>
-            </div>
-            <!-- Column -->
-            <div class="col-md-6 col-lg-3">
-                <div class="card card-hover">
-                    <a href="http://192.168.99.202/daily-new/public" target="_blank">
-                        <div class="box bg-cyan text-center">
-                            <h1 class="font-light text-white"><i class="mdi mdi-factory"></i></h1>
-                            <h6 class="text-white">BSKP Production</h6>
-                        </div>
-                    </a>
-                </div>
-            </div>
-            <!-- Column -->
-            <div class="col-md-6 col-lg-3">
-                <div class="card card-hover">
-                    <a href="http://192.168.99.202/kpi/public" target="_blank">
-                        <div class="box bg-success text-center">
-                            <h1 class="font-light text-white"><i class="mdi mdi-trophy"></i></h1>
-                            <h6 class="text-white">BSKP KPI</h6>
-                        </div>
-                    </a>
-                </div>
-            </div>
-            <!-- Column -->
-            <div class="col-md-6 col-lg-3">
-                <div class="card card-hover">
-                    <a href="http://192.168.99.202/zen-z-zai/public" target="_blank">
-                        <div class="box bg-danger text-center">
-                            <h1 class="font-light text-white"><i class="mdi mdi-file-document"></i></h1>
-                            <h6 class="text-white">Zen-Z-Zai</h6>
-                        </div>
-                    </a>
-                </div>
-            </div>
-        </div>
-        <div class="row">
-            <!-- Column -->
-            <div class="col-md-6 col-lg-3">
-                <div class="card card-hover">
-                    <a href="http://192.168.99.202/doc-app/public" target="_blank">
-                        <div class="box bg-secondary text-center">
-                            <h1 class="font-light text-white"><i class="mdi mdi-clipboard-text"></i></h1>
-                            <h6 class="text-white">BSKP SOP</h6>
-                        </div>
-                    </a>
-                </div>
-            </div>
-            <!-- Column -->
-            <div class="col-md-6 col-lg-3">
-                <div class="card card-hover">
-                    <a href="http://192.168.99.202/salary/public" target="_blank">
-                        <div class="box bg-info text-center">
-                            <h1 class="font-light text-white"><i class="mdi mdi-cash-100"></i></h1>
-                            <h6 class="text-white">BSKP Salary</h6>
-                        </div>
-                    </a>
-                </div>
-            </div>
-            <!-- Column -->
-            <div class="col-md-6 col-lg-3">
-                <div class="card card-hover">
-                    <a href="http://192.168.99.202/soilcondition/s/public" target="_blank">
-                        <div class="box bg-warning text-center">
-                            <h1 class="font-light text-white"><i class="mdi mdi-google-maps"></i></h1>
-                            <h6 class="text-white">BSKP MAP</h6>
-                        </div>
-                    </a>
-                </div>
-            </div>
-            <!-- Column -->
-            <div class="col-md-6 col-lg-3">
-                <div class="card card-hover">
-                    <a href="http://192.168.99.202/nextcloud" target="_blank">
-                        <div class="box bg-primary text-center">
-                            <h1 class="font-light text-white"><i class="mdi mdi-cloud-sync"></i></h1>
-                            <h6 class="text-white">Nextcloud</h6>
-                        </div>
-                    </a>
-                </div>
-            </div>
-        </div> --}}
-
-        <div class="row">
             <div class="col-md-12">
                 <!-- card -->
                 <div class="card">
@@ -184,24 +91,10 @@
                                 </div>
                             </div>
                         </li>
-                        <li class="d-flex no-block card-body border-top">
-                            <i class="fa fa-leaf w-30px m-t-5"></i>
-                            <div>
-                                <a href="#" class="m-b-0 font-medium p-0">Envato approved Maruti Admin
-                                    template</a>
-                                <span class="text-muted">i am very happy to approved by TF</span>
-                            </div>
-                            <div class="ml-auto">
-                                <div class="tetx-right">
-                                    <h5 class="text-muted m-b-0">20</h5>
-                                    <span class="text-muted font-16">Jan</span>
-                                </div>
-                            </div>
-                        </li>
                     </ul>
                 </div>
             </div>
-        </div>
+        </div> --}}
 
         <!-- ============================================================== -->
         <!-- End PAge Content -->
